ba452f17cba760465679b7ee239802c533c52a73
vibinroy <67194335+vibinroy@users.noreply.github.com>
2024-04-13 12:35:05 +0530

    Update streamlit_app.py
    added pandas file

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,8 +17,12 @@ st.write('The name on your Smoothie will be:', NAME_ON_ORDER)
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-st.dataframe(data=my_dataframe,use_container_width=True)
+# st.dataframe(data=my_dataframe,use_container_width=True)
+# st.stop()
+pd_df=my_dataframe.to_pandas()
+st.dataframe(pd_df)
 st.stop()
+
 # --st.dataframe(data=my_dataframe, use_container_width=True)
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients',
