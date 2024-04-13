2595485276e4ba47cae36900129c917677c5c5ec
vibinroy <67194335+vibinroy@users.noreply.github.com>
2024-04-13 12:07:59 +0530

    Update streamlit_app.py
    added json in fruityvice

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,6 +45,6 @@ st.success('Your Smoothie is ordered!',icon="✅")
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
+st.text(fruityvice_response.json())
 
 
