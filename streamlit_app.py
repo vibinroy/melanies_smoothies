90a00feed400c5341180cbb6611f6a9a1376bf31
vibinroy <67194335+vibinroy@users.noreply.github.com>
2024-04-13 12:36:18 +0530

    Update streamlit_app.py
    added 
    search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
    st.write('The search value for ', fruit_chosen,' is ', search_on, '.')

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,8 +20,8 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 # st.dataframe(data=my_dataframe,use_container_width=True)
 # st.stop()
 pd_df=my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+# st.dataframe(pd_df)
+# st.stop()
 
 # --st.dataframe(data=my_dataframe, use_container_width=True)
 ingredients_list = st.multiselect(
@@ -34,6 +34,10 @@ if ingredients_list:
     # NAME_ON_ORDER=''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen +' '
+        
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+
         st.subheader(fruit_chosen +'Nutrition Information')
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon"+ fruit_chosen)
         fv_df= st.dataframe(data=fruityvice_response.json(),use_container_width=True)
