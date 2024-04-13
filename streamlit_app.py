3860c49d41ed3de36166fe68e473efd66c789299
vibinroy <67194335+vibinroy@users.noreply.github.com>
2024-04-13 11:58:44 +0530

    Update streamlit_app.py
    added request function

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,4 +43,8 @@ if ingredients_list:
         
 st.success('Your Smoothie is ordered!',icon="✅")
 
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
+
 
