906820c361f48f0a1f68e7cd7934b393995e4bed
vibinroy <67194335+vibinroy@users.noreply.github.com>
2024-04-13 12:09:55 +0530

    Update streamlit_app.py
    added table format

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,5 +46,5 @@ st.success('Your Smoothie is ordered!',icon="✅")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 st.text(fruityvice_response.json())
-
+fv_df= st.dataframe(data=fruityvice_response.json(),use_container_width=True)
 
